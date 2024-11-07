2908f85e293e36a55ee7bf79d6d7b53c2d79b2e1
Klara Lehner <lehner.klr@gmail.com>
2024-11-07 13:50:36 +0100

    add tictactoe function

--- Homework_lesson_06.py
+++ Homework_lesson_06.py
@@ -5,6 +5,7 @@
 # "o" – The player who uses noughts (Os) has won (the board contains ooo)
 #"!" – Draw (the board is full but nobody has won)
 # "-" – Rest (i.e. the game is not finished)
+from random import randrange
 
 def evaluate (string):
     crosses = "xxx"
@@ -23,10 +24,10 @@ def evaluate (string):
 # and returns a game board  (i.e., a string with the given mark placed in the given position). 
 
 def move (string, position_number, character):
-    part_a = string[:(position_number - 1)]
-    part_b = string[position_number:]
+    part_a = string[:position_number]
+    part_b = string[position_number + 1:]
     board_update = part_a + character + part_b
-    return (board_update)
+    return board_update
 
 # Write a player_move function that accepts a string with the game board, 
 # asks the player which position he wants to play and returns the updated game board with the player's move. 
@@ -36,10 +37,9 @@ def move (string, position_number, character):
 
 def player_move (string):
     while (True):
-        position_number = int(input("Make your move!\n Tell me where to put your x (number beweteen 1 and 20): "))
-        if (position_number >= 1) & (position_number <= 20):
-            player_position = string[(position_number - 1): position_number]
-            if player_position == '-':
+        position_number = int(input("Make your move!\n Tell me where to put your x (number beweteen 1 and 20): ")) - 1
+        if 0 <= position_number < 20:
+            if string [position_number] == '-':
                 return move(string, position_number, 'x')
             else:
                 print("Someone has been here before!")
@@ -53,11 +53,40 @@ def player_move (string):
 # If the position is empty, place the computer's mark there.
 # If not, repeat from the first step (select another random number). The function header should look something like this:
 
-from random import randrange
+
 def pc_move (string):
     while (True):
-        pc_choice = 0
+        pc_choice = randrange (0,20)
         if string[pc_choice] == '-':
-            string = string[:pc_choice] + 'o' + string[pc_choice +1]
+            return move(string, pc_choice, 'o')
+
+# Write a tictactoe_1d function that creates a string with a game board and alternately calls the player_move and pc_move functions
+# until someone wins or draws. Do not forget to check the status of the game after every turn.
+
+def tictactoe ():
+    board = "--------------------"
+    turn = "player"
+
+    while (True):
+        print("\nCurrent board", board)
+        if turn == "player":
+            board = player_move(board)
+            turn = "pc"
+        else:
+            board = pc_move(board)
+            turn = "player"
+        
+        result = evaluate(board)
+        if result != "-": 
+            print("\nFinal board:", board)
+            if result == "x":
+                print("Player wins!")
+            elif result == "o":
+                print("PC wins!")
+            elif result == "!":
+                print("Draw!")
             break
-    return string
+    return board
+
+
+tictactoe()
